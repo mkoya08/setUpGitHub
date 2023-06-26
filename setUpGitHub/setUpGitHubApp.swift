f6cb4333a6ef00e54b59a45707ba4c0336228112
Moyosore Ogunkoya <momoart188@gmail.com>
2023-06-26 10:12:17 -0400

    Initial Commit

---
+++ setUpGitHub/setUpGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  setUpGitHubApp.swift
+//  setUpGitHub
+//
+//  Created by Moyo on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct setUpGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
